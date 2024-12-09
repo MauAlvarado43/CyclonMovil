542e9782cc464398f4e27cb91781d3302ede7b91
Mauricio Alvarado <mau.alvalop43@gmail.com>
2024-12-08 21:31:56 -0600

    Fix

---
+++ Cyclon/Cyclon/Utils/Cipher.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Cyclon.Utils {
+    class Cipher
+    {
+
+        private static string _key = "";
+        private static string _iv = "";
+
+        public static string encrypt(string plainText) {
+
+            byte[] encrypted;
+
+            using (Aes aes = Aes.Create()) {
+
+                aes.Key = Convert.FromBase64String(_key);
+                aes.IV = Convert.FromBase64String(_iv);
+
+                using (MemoryStream memoryStream = new MemoryStream()) {
+
+                    using (ICryptoTransform encryptor = aes.CreateEncryptor())
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+
+                    using (StreamWriter streamWriter = new StreamWriter(cryptoStream)) {
+                        streamWriter.Write(plainText);
+                    }
+
+                    encrypted = memoryStream.ToArray();
+
+                }
+            }
+
+            return Convert.ToBase64String(encrypted);
+
+        }
+
+        public static string decrypt(string encryptedValue) {
+
+            byte[] cipherText = Convert.FromBase64String(encryptedValue);
+            string plaintext = null;
+
+            using (Aes aes = Aes.Create()) {
+
+                aes.Key = Convert.FromBase64String(_key);
+                aes.IV = Convert.FromBase64String(_iv);
+
+                using (MemoryStream memoryStream = new MemoryStream(cipherText))
+                using (ICryptoTransform decryptor = aes.CreateDecryptor())
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                using (StreamReader streamReader = new StreamReader(cryptoStream))
+                
+                plaintext = streamReader.ReadToEnd();
+
+            }
+
+            return plaintext;
+
+        }
+
+
+    }
+
+}
---
+++ Cyclon/Cyclon/Utils/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cyclon.Utils
+{
+    public class Constants
+    {
+        public static string IP = "";
+    }
+}
